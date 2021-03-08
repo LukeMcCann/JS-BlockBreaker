2e9990d8b1872b5f4eb41dfa492f597732d0a775
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-08 18:24:19 +0000

    Add circle example

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 
+
+ctx.closePath();
+
+
+ctx.beginPath();
+
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fill();
+
 ctx.closePath();
